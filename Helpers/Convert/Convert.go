ba09caca60afa1511520168a306c649eeb98e3c6
Alim Gershishev <45514285+alimtolman@users.noreply.github.com>
2026-07-07 13:06:30 +0400

    Fix StrToUInt to parse up to 32 bits

--- Helpers/Convert/Convert.go
+++ Helpers/Convert/Convert.go
@@ -3,7 +3,7 @@ package Convert
 import "strconv"
 
 func StrToUInt(value string, def uint32) uint32 {
-	if number, err := strconv.ParseUint(value, 10, 0); err == nil {
+	if number, err := strconv.ParseUint(value, 10, 32); err == nil {
 		return uint32(number)
 	}
 
